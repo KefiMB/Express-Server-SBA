748d64677da307a2f2b84750b6c04a08d8daae49
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-27 19:18:47 -0400

    added middleware

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ const express = require("express")
 	const PORT = 3000
 
 	app.listen (PORT, () => {
-		console.log("Is it working?")
+		console.log("Is it working? ${PORT}")
 	});
+
+    // Middleware
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+  };
+  
+  module.exports = logger;
