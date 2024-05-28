3d5872e17d8b68d3c22da0a61f5cbd1c63d561b4
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-27 22:16:35 -0400

    Posts

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 	const PORT = 3000
 
 	app.listen (PORT, () => {
-		console.log("Is it working? ${PORT}")
+		console.log("Is it working?")
 	});
 
     // Middleware
@@ -13,3 +13,21 @@ const logger = (req, res, next) => {
   };
   
   module.exports = logger;
+  
+const isVegan = (req, res, next) => {
+    if (req.body.isVegan !== undefined && req.body.isVegan === false) {
+      return res.status(400).json({ message: 'Recipe must be vegan' });
+    }
+    next();
+  };
+  
+  module.exports = isVegan;
+
+const logger = require('./middleware/logger');
+const isVegan = require('./middleware/isVegan');
+
+app.use(express.json());
+app.use(logger); 
+// Routes
+app.use('/api/plant-based-recipes', isVegan, plantBasedRecipesRouter); // Check if recipe is vegan
+
