40f4342def001ba48c1e7bad6072c7ae7b750a35
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-27 22:17:26 -0400

    Users

---
+++ data/users.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+      id: 1,
+      name: 'ntokozo',
+      username: 'ntk24',
+      email: 'ntkz@example.com',
+    },
+    {
+      id: 2,
+      name: 'casey',
+      username: 'Kasee',
+      email: 'casey98@example.com',
+    },
+    {
+      id: 3,
+      name: 'Mandla',
+      username: 'ManMan',
+      email: 'm_dlamini@example.com',
+    },
+  ];
+  
+  module.exports = users;
